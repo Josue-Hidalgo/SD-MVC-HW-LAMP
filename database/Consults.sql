98ec69537d86d7fbafe0c0d8bcdcad31ba07efe6
Josue Hidalgo <josuehidalgos2511@gmail.com>
2026-03-18 16:53:07 -0600

    Update Consults.sql

--- database/Consults.sql
+++ database/Consults.sql
@@ -1,51 +1,56 @@
--- NOTE THAT: 
--- If you will use any of these queries, make sure to replace the '?' with the actual shortcut_id you want to analyze.
+-- NOTE THAT: -- If you will use any of these queries, make sure to replace the '?' with the actual shortcut_id you want to analyze. 
 -- So watch out!! xd
 
 -- Total Accesses 
-SELECT COUNT(*) FROM AccessLog;
+SELECT COUNT(*) AS total_accesses
+FROM AccessLog;
+
 
 -- Total Accesses Per URL
 SELECT COUNT(*) AS total_access
 FROM AccessLog
-WHERE shortcut_id = ?;
+WHERE id_short_cut = ?;
+
 
 -- Total Accesses Per Country
 SELECT country, COUNT(*) AS total
 FROM AccessLog
-WHERE shortcut_id = ?
+WHERE id_short_cut = ?
 GROUP BY country
 ORDER BY total DESC;
+
 
 -- Total Accesses Per Day
 SELECT DATE(access_date) AS day, COUNT(*) AS total
 FROM AccessLog
-WHERE shortcut_id = ?
+WHERE id_short_cut = ?
 GROUP BY day
 ORDER BY day;
+
 
 -- Countries 
 SELECT country, COUNT(*) AS total
 FROM AccessLog
-WHERE shortcut_id = ?
+WHERE id_short_cut = ?
 GROUP BY country;
+
 
 -- Delete all data (use with caution!!)
 DELETE FROM AccessLog;
 DELETE FROM ShortCut;
 
--- Increase the access count
-UPDATE ShortCut
-SET access_count = access_count + 1
-WHERE id = ?;
 
--- Insert a new access log entry
-INSERT INTO AccessLog (shortcut_id, access_date, country)
-VALUES (?, NOW(), ?);
+-- Insert a new access log entry (usando short_code directamente)
+INSERT INTO AccessLog (id_short_cut, ip_address, country)
+SELECT id_short_cut, ?, ?
+FROM ShortCut
+WHERE short_code = ?;
+
 
 -- Insert a new ShortCut entry
 INSERT INTO ShortCut (short_code, original_url, base_url)
 VALUES (?, ?, ?);
+
 
 -- Get id_shortcut when user is trying to access a short URL
 SELECT id_short_cut
